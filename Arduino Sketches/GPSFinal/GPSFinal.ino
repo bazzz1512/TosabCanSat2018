852035619c20bb1752d6d221875a167752027798
bazzz1512 <bazzz1512@users.noreply.github.com>
2018-03-06 13:21:51 +0100

    Update GPSFinal.ino

--- Arduino Sketches/GPSFinal/GPSFinal.ino
+++ Arduino Sketches/GPSFinal/GPSFinal.ino
@@ -2,13 +2,12 @@
 
 #include <TinyGPS.h>
 
-/* This sample code demonstrates the normal use of a TinyGPS object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+/* 
+Final code for our GPS tracker
 */
-
+//Create SoftwareSerial ports
 TinyGPS gps;
-SoftwareSerial ss(6, 7);
+SoftwareSerial ss(6, 7); //ss stands for softwareserial and is our direct connection with the gps module
 SoftwareSerial HC12(3, 2);
 
 void setup()
@@ -17,7 +16,7 @@ void setup()
    HC12.begin(9600);
    ss.begin(9600);
 
-  
+  //Output test sentences to Serial and to HC12
   Serial.print("Simple TinyGPS library v. "); Serial.println(TinyGPS::library_version());
   Serial.println("by Mikal Hart");
   Serial.println();
@@ -34,7 +33,7 @@ ss.listen();
     while (ss.available())
     {
       char c = ss.read();
-      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      // Serial.write(c); // Raw GPS data
       if (gps.encode(c)) // Did a new valid sentence come in?
         newData = true;
     }
@@ -42,6 +41,7 @@ ss.listen();
 
   if (newData)
   {
+    //Prepare data to be sent with the HC12 
     HC12.listen();
    
     float flat, flon;
@@ -49,11 +49,11 @@ ss.listen();
     gps.f_get_position(&flat, &flon, &age);
     HC12.print(F("L= "));
     Serial.print(F("L= "));
-    HC12.println(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, DEC);
+    HC12.println(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, DEC); //Check if the coordinates are avaible, if so, send them
     Serial.println(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, DEC);
     HC12.print(F("B= "));
     Serial.print(F("B= "));
-    HC12.println(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon,DEC);
+    HC12.println(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon,DEC); //check if the coordinates are avaible, if so, send them
     Serial.println(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, DEC);
     Serial.println();
     HC12.flush();
